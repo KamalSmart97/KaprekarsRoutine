80b94e86ec5dc96be848697eeeba91d35d2084a4
KamalSmart97 <123406583+KamalSmart97@users.noreply.github.com>
2024-04-21 15:14:13 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include"hash.h"
 
-#include <pthread.h>
+#include <thread>
 #include <stdio.h>
+#include<iostream>
 #include <stdlib.h>
+
+#define NO_OF_DIGITS 6
+
+using namespace std;
+
 
 //order 1 is ascending
 //order 2 is descending
@@ -11,7 +17,7 @@ unsigned long long int ArrangeInOrder(unsigned long long int nNumber, unsigned l
 {
 	int temp = 0, nCount = 0;
 	int arr[10] = { 0 };
-	unsigned long long int nArrangedNumber = 0llu, nAsceding = 0llu, nDescending=0llu;
+	unsigned long long int nArrangedNumber = 0llu, nAsceding = 0llu, nDescending = 0llu;
 
 	while (nNumber > 0)
 	{
@@ -44,17 +50,18 @@ unsigned long long int ArrangeInOrder(unsigned long long int nNumber, unsigned l
 	return 0;
 }
 
-int kaprekarCycle(unsigned long long int nStartValue, unsigned long long int nEndValue, char * fileName)
+void kaprekarCycle(unsigned long long int nStartValue, unsigned long long int nEndValue, string fileName)
 {
 	unsigned long long int nNumber = 0llu, nAscending = 0llu, nDescedning = 0llu, ntemp = 0llu;
 	bool bNewCycle = false;
 	FILE* fp = NULL;
-	fp = fopen(fileName, "w+");
+	const char* myCharPtr = fileName.c_str();
+	fp = fopen(myCharPtr, "w+");
 	hashMap actualCyclehash;
 
 	for (unsigned long long int nNumber = nStartValue; nNumber < nEndValue; nNumber++)
 	{
-3		hashMap temphash;
+		hashMap temphash;
 		bNewCycle = true;
 		ntemp = nNumber;
 		while (temphash.insert(ntemp))
@@ -64,7 +71,7 @@ int kaprekarCycle(unsigned long long int nStartValue, unsigned long long int nEn
 				bNewCycle = false;
 				break;
 			}
-			ArrangeInOrder(ntemp, &nAscending, &nDescedning,11);
+			ArrangeInOrder(ntemp, &nAscending, &nDescedning, NO_OF_DIGITS);
 			ntemp = nDescedning - nAscending;
 		}
 		if ((ntemp > 0) && bNewCycle)
@@ -72,58 +79,35 @@ int kaprekarCycle(unsigned long long int nStartValue, unsigned long long int nEn
 			fprintf(fp, "\n Number: %llu \n", nNumber);
 			while (actualCyclehash.insert(ntemp))
 			{
-				ArrangeInOrder(ntemp, &nAscending, &nDescedning, 11);
+				ArrangeInOrder(ntemp, &nAscending, &nDescedning, NO_OF_DIGITS);
 				ntemp = nDescedning - nAscending;
 				fprintf(fp, "%llu ->", ntemp);
 			}
-			fprintf(fp,"\n", &ntemp);
+			fprintf(fp, "\n", &ntemp);
 		}
 	}
 	if (fp)
 		fclose(fp);
 
-	return 0;
-}
-
-// Define a structure to pass parameters to the threads
-typedef struct {
-	unsigned long long int start;
-	unsigned long long int end;
-	char* fileName;
-} ThreadParams;
-
-// The thread function that calls kaprekarCycle
-void* threadFunction(void* arg) {
-	ThreadParams* params = (ThreadParams*)arg;
-	kaprekarCycle(params->start, params->end, params->fileName);
-	return NULL;
+	return;
 }
 
 int main() {
-	const int NUM_THREADS = 4; // Number of threads to create
-	pthread_t threads[NUM_THREADS];
-	ThreadParams threadParams[NUM_THREADS];
-	char* fileNames[NUM_THREADS] = { "output1.txt", "output2.txt", "output3.txt", "output4.txt" };
-
+	const int NUM_THREADS = 20; // Number of threads to create
+	thread threads[NUM_THREADS];
+	string fileNames[NUM_THREADS] = { "D:/project/kaprekarsroutine/sampleFIle/output1.txt", "D:/project/kaprekarsroutine/sampleFIle/output2.txt", "D:/project/kaprekarsroutine/sampleFIle/output3.txt", "D:/project/kaprekarsroutine/sampleFIle/output4.txt","D:/project/kaprekarsroutine/sampleFIle/output5.txt","D:/project/kaprekarsroutine/sampleFIle/output6.txt","D:/project/kaprekarsroutine/sampleFIle/output7.txt","D:/project/kaprekarsroutine/sampleFIle/output8.txt","D:/project/kaprekarsroutine/sampleFIle/output9.txt","D:/project/kaprekarsroutine/sampleFIle/output10.txt","D:/project/kaprekarsroutine/sampleFIle/output11.txt","D:/project/kaprekarsroutine/sampleFIle/output12.txt", "D:/project/kaprekarsroutine/sampleFIle/output13.txt", "D:/project/kaprekarsroutine/sampleFIle/output14.txt", "D:/project/kaprekarsroutine/sampleFIle/output15.txt", "D:/project/kaprekarsroutine/sampleFIle/output16.txt", "D:/project/kaprekarsroutine/sampleFIle/output17.txt", "D:/project/kaprekarsroutine/sampleFIle/output18.txt", "D:/project/kaprekarsroutine/sampleFIle/output19.txt","D:/project/kaprekarsroutine/sampleFIle/output20.txt" };
+	unsigned long long int nStart = 0, nEnd = 0;
 	// Assuming you want to process numbers from 1 to 1000000
 	unsigned long long int range = 1000000 / NUM_THREADS;
 	for (int i = 0; i < NUM_THREADS; i++) {
-		threadParams[i].start = i * range + 1;
-		threadParams[i].end = (i + 1) * range;
-		threadParams[i].fileName = fileNames[i];
-
-		if (pthread_create(&threads[i], NULL, threadFunction, &threadParams[i])) {
-			fprintf(stderr, "Error creating thread\n");
-			return 1;
-		}
+		nStart = i * range + 1;
+		nEnd = (i + 1) * range;
+		threads[i] = thread(kaprekarCycle, ref(nStart), ref(nEnd), ref(fileNames[i]));
 	}
 
 	// Wait for all threads to finish
 	for (int i = 0; i < NUM_THREADS; i++) {
-		if (pthread_join(threads[i], NULL)) {
-			fprintf(stderr, "Error joining thread\n");
-			return 2;
-		}
+		threads[i].join();
 	}
 
 	printf("Kaprekar cycles computation completed.\n");
